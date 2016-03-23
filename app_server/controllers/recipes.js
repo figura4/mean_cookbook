bf86d1441356a3a13ceaff19e21ad13ebaf6baaf
figura4 <oscar.riva@gmail.com>
2016-03-23 10:40:59 +0000

    moved data to controller

--- app_server/controllers/recipes.js
+++ app_server/controllers/recipes.js
@@ -1,11 +1,83 @@
 /* GET 'home' page */
 module.exports.homelist = function(req, res){
-  res.render('recipe-list', { title: 'Home' });
+  res.render('recipe-list', { 
+    title: 'Mean cookbook - find recipes for you',
+    pageHeader: {
+      title: 'Mean cookbook',
+      strapline: 'find recipes for you'
+    },
+    recipes: [{
+      name: 'Carbonara',
+      description: 'pasta alla carbonara',
+      rating: 5,
+      tags: [
+        'primo',
+        'pasta',
+        'porcello',
+      ],
+      ingredients: [{
+        name: 'pasta',
+        measure: 'g',
+        quantity: '400'
+      }, {
+        name: 'porcello',
+        measure: 'g',
+        quantity: '500'
+      }],
+    }, {
+      name: 'Puttanesca',
+      description: 'pasta alla puttanesca',
+      rating: 4,
+      tags: [
+        'primo',
+        'pasta',
+        'no porcello',
+      ],
+      ingredients: [{
+        name: 'pasta',
+        measure: 'g',
+        quantity: '300'
+      }, {
+        name: 'olive',
+        measure: 'g',
+        quantity: '300'
+      }],
+    }]
+  });
 };
 
 /* GET 'Recipe info' page */
 module.exports.recipeInfo = function(req, res){
-  res.render('recipe-info', { title: 'Recipe info' });
+  res.render('recipe-info', {
+    title: 'Mean cookbook - Carbonara',
+    pageHeader: {
+      title: 'Carbonara',
+    },
+    recipe: {
+      name: 'Carbonara',
+      description: 'pasta alla carbonara',
+      rating: 5,
+      tags: [
+        'primo',
+        'pasta',
+        'porcello',
+      ],
+      ingredients: [{
+        name: 'pasta',
+        measure: 'g',
+        quantity: '400'
+      }, {
+        name: 'porcello',
+        measure: 'g',
+        quantity: '500'
+      }],
+      steps: [
+        'butta la pasta',
+        'rompi le uova',
+        'picchia il porcello'
+      ]
+    },
+  });
 };
 
 /* GET 'Add ingredient' page */
