b99f5b8b3f8248dfca7a822d1f3504598ff7c3f0
figura4 <oscar.riva@gmail.com>
2016-03-24 13:08:54 +0000

    ingredients crud ok

--- app_api/controllers/ingredients.js
+++ app_api/controllers/ingredients.js
@@ -52,6 +52,145 @@ module.exports.ingredientsCreate = function (req, res) {
 };
 
 
-module.exports.ingredientsReadOne = function (req, res) { };
-module.exports.ingredientsUpdateOne = function (req, res) { };
-module.exports.ingredientsDeleteOne = function (req, res) { };
+module.exports.ingredientsReadOne = function (req, res) { 
+  console.log("Getting single ingredient");
+  if (req.params && req.params.recipeid && req.params.ingredientid) {
+    Rec
+      .findById(req.params.recipeid)
+      .select('name ingredients')
+      .exec(
+        function(err, recipe) {
+          console.log(recipe);
+          var response, ingredient;
+          if (!recipe) {
+            sendJsonResponse(res, 404, {
+              "message": "recipeid not found"
+            });
+            return;
+          } else if (err) {
+            sendJsonResponse(res, 400, err);
+            return;
+          }
+          if (recipe.ingredients && recipe.ingredients.length > 0) {
+            ingredient = recipe.ingredients.id(req.params.ingredientid);
+            if (!ingredient) {
+              sendJsonResponse(res, 404, {
+                "message": "ingredientid not found"
+              });
+            } else {
+              response = {
+                recipe: {
+                  name: recipe.name,
+                  id: req.params.recipeid
+                },
+                ingredient: ingredient
+              };
+              sendJsonResponse(res, 200, response);
+            }
+          } else {
+            sendJsonResponse(res, 404, {
+              "message": "No ingredients found"
+            });
+          }
+        }
+    );
+  } else {
+    sendJsonResponse(res, 404, {
+      "message": "Not found, recipeid and locationid are both required"
+    });
+  }
+};
+
+module.exports.ingredientsUpdateOne = function (req, res) { 
+  if (!req.params.recipeid || !req.params.ingredientid) {
+    sendJsonResponse(res, 404, {
+      "message": "Not found, recipeid and ingredientid are both required"
+    });
+    return;
+  }
+  Rec
+    .findById(req.params.recipeid)
+    .select('ingredients')
+    .exec(
+      function(err, recipe) {
+        var thisIngredient;
+        if (!recipe) {
+          sendJsonResponse(res, 404, {
+            "message": "recipeid not found"
+          });
+          return;
+        } else if (err) {
+          sendJsonResponse(res, 400, err);
+          return;
+        }
+        if (recipe.ingredients && recipe.ingredients.length > 0) {
+          thisIngredient = recipe.ingredients.id(req.params.ingredientid);
+          if (!thisIngredient) {
+            sendJsonResponse(res, 404, {
+              "message": "ingredientid not found"
+            });
+          } else {
+            thisIngredient.name = req.body.name;
+            thisIngredient.measure = req.body.measure;
+            thisIngredient.quantity = req.body.quantity;
+            recipe.save(function(err, recipe) {
+              if (err) {
+                sendJsonResponse(res, 404, err);
+              } else {
+                sendJsonResponse(res, 200, thisIngredient);
+              }
+            });
+          }
+        } else {
+          sendJsonResponse(res, 404, {
+            "message": "No ingredient to update"
+          });
+        }
+      }
+  );
+};
+
+module.exports.ingredientsDeleteOne = function (req, res) { 
+  if (!req.params.recipeid || !req.params.ingredientid) {
+    sendJsonResponse(res, 404, {
+      "message": "Not found, recipeid and ingredientid are both required"
+    });
+    return;
+  }
+  Rec
+    .findById(req.params.recipeid)
+    .select('ingredients')
+    .exec(
+      function(err, recipe) {
+        if (!recipe) {
+          sendJsonResponse(res, 404, {
+            "message": "recipeid not found"
+          });
+          return;
+        } else if (err) {
+          sendJsonResponse(res, 400, err);
+          return;
+        }
+        if (recipe.ingredients && recipe.ingredients.length > 0) {
+          if (!recipe.ingredients.id(req.params.ingredientid)) {
+            sendJsonResponse(res, 404, {
+              "message": "recipeid not found"
+            });
+          } else {
+            recipe.ingredients.id(req.params.ingredientid).remove();
+            recipe.save(function(err) {
+              if (err) {
+                sendJsonResponse(res, 404, err);
+              } else {
+                sendJsonResponse(res, 204, null);
+              }
+            });
+          }
+        } else {
+          sendJsonResponse(res, 404, {
+            "message": "No ingredient to delete"
+          });
+        }
+      }
+  );
+};
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -4,16 +4,20 @@ var ctrlRecipes = require('../controllers/recipes');
 var ctrlIngredients = require('../controllers/ingredients');
 var ctrlSteps = require('../controllers/steps');
 
+/* recipes */
 router.post('/recipes', ctrlRecipes.recipesCreate);
 router.get('/recipes/:recipeid', ctrlRecipes.recipesReadOne);
 router.put('/recipes/:recipeid', ctrlRecipes.recipesUpdateOne);
 router.delete('/recipes/:recipeid', ctrlRecipes.recipesDeleteOne);
 
+/* ingredients */
 router.post('/recipes/:recipeid/ingredients', ctrlIngredients.ingredientsCreate);
 router.get('/recipes/:recipeid/ingredients/:ingredientid', ctrlIngredients.ingredientsReadOne);
 router.put('/recipes/:recipeid/ingredients/:ingredientid', ctrlIngredients.ingredientsUpdateOne);
-router.delete('/reviews/:reviewid/ingredients/:ingredientid', ctrlIngredients.ingredientsDeleteOne);
+router.delete('/recipes/:recipeid/ingredients/:ingredientid', ctrlIngredients.ingredientsDeleteOne);
 
+
+/* steps */
 router.post('/recipes/:recipeid/steps', ctrlSteps.stepsCreate);
 router.get('/recipes/:recipeid/steps/:stepsid', ctrlSteps.stepsReadOne);
 router.put('/recipes/:recipeid/steps/:stepsid', ctrlSteps.stepsUpdateOne);
